82279697ef02b9f9548e7267504b018aacccba3b
Georgiy Podobin <gpodobin@yandex.ru>
2026-02-20 14:23:00 +0300

    Complete task 3

--- src/main/java/tasks/Task3.java
+++ src/main/java/tasks/Task3.java
@@ -1,9 +1,9 @@
 package tasks;
 
 import common.Person;
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /*
 Задача 3
@@ -12,6 +12,23 @@ import java.util.List;
 public class Task3 {
 
   public static List<Person> sort(Collection<Person> persons) {
-    return new ArrayList<>(persons);
+    return persons.stream()
+        .sorted((p1, p2) -> {
+          if (p1.secondName().equals(p2.secondName()))
+            if (p1.firstName().equals(p2.firstName()))
+              return p1.createdAt().compareTo(p2.createdAt());
+            else
+              return p1.firstName().compareTo(p2.firstName());
+          else
+            return p1.secondName().compareTo(p2.secondName());
+        })
+        .collect(Collectors.toList());
   }
 }
+/*
+Объявляю свой компаратор через лямбда-выражения.
+Сравнение объектов происходит по следующему принципу.
+Если фамилии равны, сравниваем имена, иначе сравниваем фамилии.
+Если равны имена, сравниваем даты, иначе сравниваем имена.
+Таким образом достигается нужный порядок при сортировке.
+ */
