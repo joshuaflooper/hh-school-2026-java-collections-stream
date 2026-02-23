920b87295e2972b46d6505157cc79ab7f810efb2
Georgiy Podobin <gpodobin@yandex.ru>
2026-02-23 12:59:10 +0300

    Complete task 8

--- src/main/java/tasks/Task8.java
+++ src/main/java/tasks/Task8.java
@@ -4,8 +4,9 @@ import common.Person;
 import common.PersonService;
 import common.PersonWithResumes;
 import common.Resume;
-import java.util.Collection;
-import java.util.Set;
+
+import java.util.*;
+import java.util.stream.Collectors;
 
 /*
   Еще один вариант задачи обогащения
@@ -21,7 +22,32 @@ public class Task8 {
   }
 
   public Set<PersonWithResumes> enrichPersonsWithResumes(Collection<Person> persons) {
-    Set<Resume> resumes = personService.findResumes(Set.of());
-    return Set.of();
+    Set<Resume> resumes = personService.findResumes(persons.stream()
+            .map(Person::id)
+            .collect(Collectors.toSet()));
+    Map<Integer, Set<Resume>> personResumesMap = new HashMap<>();
+    resumes.forEach(r -> {
+      if (personResumesMap.containsKey(r.personId()))
+        personResumesMap.get(r.personId()).add(r);
+      else
+        personResumesMap.put(r.personId(), new HashSet<Resume>(Set.of(r)));
+    });
+
+    return persons.stream()
+        .map(p -> new PersonWithResumes(p, personResumesMap.getOrDefault(p.id(), Collections.emptySet())))
+        .collect(Collectors.toSet());
   }
 }
+/*
+Не знаю, возсожно какое-то костыльное решение. Не придумал, как можно проще.
+Методом тыка выяснил, что обработка вызовов всех методов, которые лежат в пакете common,
+происходит вручную. То есть никакой прописанной логики нет. Поэтому не получится написать
+personService.findResumes(set.of(person.id))), чтобы получить множество резюме конкретной персоны.
+А так задача решалась бы в одну строчку. Короче, получаю множество всех резюме всех персон,
+преобразуя коллекцию персон в множество их id через map. Потом создаю словарь id персоны/список его резюме.
+Прохожусь по полученному ранее множеству, смотрю на id персоны-владельца резюме.
+Если я уже добавлял для него резюме в словарь, то просто к соответствующему множеству добавляю это резюме,
+иначе создаю новое множесвто из этого резюме. Потом через map превращаю коллекцию персон в множество объектов
+персона + множество её резюме. Используюю getOrDefault, чтобы получить не null, а пустое множество
+в случае, если персоны нет в словаре.
+ */
