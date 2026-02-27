e2629b0571b118eefce3023a469dab1bdd498a91
Georgiy Podobin <gpodobin@yandex.ru>
2026-02-27 10:45:37 +0300

    Fix task 2!

--- src/main/java/tasks/Task2.java
+++ src/main/java/tasks/Task2.java
@@ -1,11 +1,11 @@
 package tasks;
 
 import common.Person;
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /*
 Задача 2
@@ -18,13 +18,10 @@ public class Task2 {
   public static List<Person> combineAndSortWithLimit(Collection<Person> persons1,
                                                      Collection<Person> persons2,
                                                      int limit) {
-    List<Person> result = new ArrayList<>();
-    result.addAll(persons1);
-    result.addAll(persons2);
-    result.sort(Comparator.comparing(Person::createdAt));
-    return result.stream()
-        .limit(limit)
-        .collect(Collectors.toList());
+    return Stream.concat(persons1.stream(), persons2.stream())
+            .sorted(Comparator.comparing(Person::createdAt))
+            .limit(limit)
+            .collect(Collectors.toList());
   }
 }
 /* Создаю новый список, добавляю в его конец сначала элементы коллекции persons1,
